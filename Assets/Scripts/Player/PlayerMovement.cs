38ed2b606530b9f7a22aaaff8bfdcdffddfc011b
lakshay115080 <lakshit.m04@gmail.com>
2026-08-30 17:17:48 +0530

    Initial Commit

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    [Header("Speed Settings")]
+    [SerializeField] float walkSpeed = 10f;
+    [SerializeField] float sprintSpeed = 15f;
+    float playerSpeed;
+    bool isSprinting;
+    Vector2 moveValue;
+    [Header("Mouse Settings")]
+    [SerializeField] float mouseSensitivity = 1;
+    Transform vCam;
+    Vector2 lookValue;
+    float pitch;
+    [Header("Jump Settings")]
+    [SerializeField] float jumpValue;
+    [SerializeField] GameObject groundCheck;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] float checkRadius = 0.4f;
+    bool isJumping;
+    bool isGrounded;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Start()
+    {
+        vCam = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        HandleLook();
+        HandleSprint();
+        HandleJump();
+    }
+
+    void FixedUpdate()
+    {
+        HandleMove();
+    }
+
+    private void OnMove(InputValue value)
+    {
+        moveValue = value.Get<Vector2>();
+    }
+
+    private void HandleMove()
+    {
+        Vector3 currentPos = rb.position;
+        Vector3 moveDirection = transform.forward * moveValue.y + transform.right * moveValue.x;
+        Vector3 newPos = currentPos + moveDirection * (playerSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+    }
+
+    private void OnLook(InputValue value)
+    {
+        lookValue = value.Get<Vector2>();
+    }
+
+    private void HandleLook()
+    {
+        pitch -= lookValue.y;
+        pitch = Mathf.Clamp(pitch, -90, 90);
+        vCam.localRotation = Quaternion.Euler(pitch, 0, 0);
+        transform.Rotate(0, lookValue.x * mouseSensitivity, 0);
+    }
+
+    private void OnSprint(InputValue value)
+    {
+        isSprinting = value.isPressed;
+    }
+
+    private void HandleSprint()
+    {
+        if (isSprinting)
+        {
+            playerSpeed = sprintSpeed;
+        }
+        else
+        {
+            playerSpeed = walkSpeed;
+        }
+    }
+
+    private void OnJump(InputValue value)
+    {
+        isJumping = value.isPressed;
+    }
+
+    private void HandleJump()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.transform.position, checkRadius, groundLayer);
+        if (isGrounded && isJumping)
+        {
+            rb.AddForce(Vector3.up * jumpValue, ForceMode.Impulse);
+        }
+    }
+}
