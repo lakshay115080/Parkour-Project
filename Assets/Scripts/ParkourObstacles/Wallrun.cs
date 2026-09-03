e631731bbc730bede099b5a7636cce618f71c250
lakshay115080 <lakshit.m04@gmail.com>
2026-09-04 01:53:56 +0530

    Fixed wallRun
    No more buggy wallrun, Next is wallrun animation

--- Assets/Scripts/ParkourObstacles/Wallrun.cs
+++ Assets/Scripts/ParkourObstacles/Wallrun.cs
@@ -1,55 +1,64 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Wallrun : ParkourObstacles
 {
-    [SerializeField] float wallRunDuration = 0.5f;
+    [SerializeField] BoxCollider[] triggers;
     [SerializeField] float wallRunSpeed = 10f;
-    float wallLength;
     bool isWallrunning;
+    int currentTrigger;
 
-    void Awake()
+    void Update()
     {
-        wallLength = gameObject.transform.localScale.z;
+        if (canParkour && playerInField != null)
+        {
+            for (int i = 0; i < triggers.Length; i++)
+            {
+                if (triggers[i].bounds.Contains(playerInField.transform.position))
+                {
+                    currentTrigger = i;
+                    break;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Parkour();
+            }
+        }
     }
+
     protected override void Parkour()
     {
         if (isWallrunning) return;
 
-        Vector3 playerForward = playerInField.transform.forward;
-        playerForward.y = 0f;
-        playerForward.Normalize();
+        if (currentTrigger > triggers.Length) return;
 
-        float dot = Vector3.Dot(playerForward, transform.forward);
-
-        if (Mathf.Abs(dot) < 0.7f)
-            return;
-
-        Vector3 wallDirection = transform.forward;
-
-        if (dot < 0f)
-            wallDirection = -transform.forward;
-
-        StartCoroutine(WallRun(wallDirection));
+        StartCoroutine(WallRun());
     }
 
-    IEnumerator WallRun(Vector3 wallDirection)
+    IEnumerator WallRun()
     {
         isWallrunning = true;
         playerMovement.enabled = false;
         Rigidbody rb = playerInField.GetComponent<Rigidbody>();
         rb.isKinematic = true;
         rb.useGravity = false;
-        float time = 0;
-
-        while (time < wallRunDuration)
+        Vector3 target;
+        if (currentTrigger > 0)
         {
-            time += Time.fixedDeltaTime;
+            target = triggers[0].bounds.center;
+        }
+        else
+        {
+            target = triggers[currentTrigger + 1].bounds.center;
+        }
+        target.y = rb.position.y;
 
-            Vector3 position = rb.position;
-            position += wallDirection * wallRunSpeed * Time.fixedDeltaTime;
-
-            rb.MovePosition(position);
+        while (Vector3.Distance(rb.position, target) > 0.5f)
+        {
+            Vector3 direction = (target - rb.position).normalized;
+            rb.MovePosition(rb.position + direction * wallRunSpeed * Time.fixedDeltaTime);
 
             yield return new WaitForFixedUpdate();
         }
